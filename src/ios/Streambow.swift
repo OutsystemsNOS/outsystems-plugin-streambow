12d8577521c1018579f2236722498026c07e44f4
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2022-11-03 11:11:00 +0000

    Update Streambow.swift

--- src/ios/Streambow.swift
+++ src/ios/Streambow.swift
@@ -36,8 +36,6 @@ class Streambow: CDVPlugin, CLLocationManagerDelegate {
                     print("\n>>> Test done <<<\n")
                 } else {
                     print("\n>>> Not registered <<<\n")
-                    self.pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "Error: Not registered")
-                    self.commandDelegate!.send(pluginResult, callbackId: self.pluginCommand.callbackId)
                 }
             }
         } else {
@@ -100,3 +98,4 @@ enum TestType: String {
     case upload = "upload"
     case ping = "ping"
 }
+
